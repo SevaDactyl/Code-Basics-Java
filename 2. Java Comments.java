f1f241d8a51b20ddf16471c9d4e91553bbbd0e4b
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-12-28 22:15:23 +0500

    Java: Comments

---
+++ 2. Java Comments.java
@@ -0,0 +1,11 @@
+/*
+Создайте однострочный комментарий с текстом: You know nothing, Jon Snow!. 
+*/
+
+class App {
+    public static void main(String[] args) {
+        // BEGIN
+        // You know nothing, Jon Snow!
+        // END
+    }
+}
