4738d95fff7e5df39ba2764cf0f6f3814833b0d8
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-04 23:56:51 +0500

    Java: Instructions (Statements)

---
+++ 3. Java Instructions (Statements).java
@@ -0,0 +1,18 @@
+/*
+Выведите на экран друг за другом три имени: Robert, Stannis, Renly. В результате на экране должно отобразиться:
+
+Robert
+Stannis
+Renly
+Для каждого имени используйте свой собственный вызов System.out.println().
+*/
+
+class App {
+    public static void main(String[] args) {
+        // BEGIN
+        System.out.println("Robert");
+        System.out.println("Stannis");
+        System.out.println("Renly");
+        // END
+    }
+}
