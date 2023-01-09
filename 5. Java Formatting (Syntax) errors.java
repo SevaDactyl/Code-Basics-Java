4ca081b3d3a678b0ec855c756419d939af6320ad
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-09 22:52:06 +0500

    Java: Formatting (Syntax) errors

---
+++ 5. Java Formatting (Syntax) errors.java
@@ -0,0 +1,14 @@
+/*
+Это задание не связано с уроком напрямую. Но будет полезным потренироваться с выводом на экран. Выведите на экран What Is Dead May Never Die.
+*/
+
+
+class App {
+
+    public static void main(String[] args) {
+        // BEGIN
+        System.out.println("What Is Dead May Never Die");
+        // END
+    }
+
+}
