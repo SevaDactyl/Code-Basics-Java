ee4a754f13b9ccbf328cb5cb4cb0989b8e564850
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-10 23:55:16 +0500

    Java: Arithmetic operations

---
+++ 6. Java Arithmetic operations.java
@@ -0,0 +1,12 @@
+/*
+Выведите на экран результат деления числа 81 на 9.
+*/
+
+
+class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println(81 / 9);
+        // END
+    }
+}
