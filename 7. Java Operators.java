f9da3f976331e8723b6a26f065821e1d30594648
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-11 22:36:16 +0500

    Java: Operators

---
+++ 7. Java Operators.java
@@ -0,0 +1,14 @@
+/*
+Напишите программу, которая посчитает разность между числами 6 и -81 и выведет ответ на экран.
+*/
+
+
+class App {
+
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println(6 - (- 81));
+        // END
+    }
+
+}
