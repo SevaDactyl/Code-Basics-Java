b78b98c08c7e0692b5081e13ac92c028189510fb
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-15 23:03:10 +0500

    Java: Floating Point Numbers

---
+++ 11. Java Floating Point Numbers.java
@@ -0,0 +1,12 @@
+/*
+Вычислите и выведите на экран произведение двух чисел: 0.39 и 0.22
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println(0.39 * 0.22);
+        // END
+    }
+}
