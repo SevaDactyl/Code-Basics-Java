825fda306fcaccce1e6fb37b1ccbdffc45ea718c
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-17 23:56:07 +0500

    Java: Quotes

---
+++ 13. Java Quotes.java
@@ -0,0 +1,15 @@
+/*
+Напишите программу, которая выведет на экран:
+
+"Khal Drogo's favorite word is "athjahakar""
+Программа должна вывести на экран эту фразу в точности. Обратите внимание на кавычки в начале и в конце фразы.
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println("\"Khal Drogo's favorite word is \"athjahakar\"\"");
+        // END
+    }
+}
