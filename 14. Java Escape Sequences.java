ca825905db903a5005f6f4cc4a80b15bddaf61af
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-18 23:35:06 +0500

    Java: Escape Sequences

---
+++ 14. Java Escape Sequences.java
@@ -0,0 +1,16 @@
+/*
+Напишите программу, которая выводит на экран:
+
+- Did Joffrey agree?
+- He did. He also said "I love using \n".
+При этом программа использует только один System.out.println(), но результат на экране должен выглядеть в точности как показано выше.
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".");
+        // END
+    }
+}
