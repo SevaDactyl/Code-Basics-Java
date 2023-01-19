4040d581958fc430147bda2c1994772998e3a12b
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-19 23:50:03 +0500

    Java: Concatenation

---
+++ 15. Java Concatenation.java
@@ -0,0 +1,15 @@
+/*
+Используя System.out.println(), выполните конкатенацию слов и выведите на экран фразу:
+
+Winter came for the House of Frey.
+Каждое слово должно быть записано отдельно и взято в кавычки, например "Winter ". Не забудьте о пробелах после каждого слова.
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN
+        System.out.println("Winter " + "came " + "for " + "the " + "House " + "of " + "Frey.");
+        // END
+    }
+}
