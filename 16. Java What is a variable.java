7d8316955f8f2f78c4164ca7f033b0f4b4416efc
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-20 23:58:33 +0500

    Java: What is a variable

---
+++ 16. Java What is a variable.java
@@ -0,0 +1,16 @@
+/*
+Создайте переменную с именем motto и содержимым What Is Dead May Never Die!. Распечатайте содержимое переменной.
+
+
+What Is Dead May Never Die!
+
+*/
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        var motto = "What Is Dead May Never Die!";
+        System.out.println(motto);
+        // END
+    }
+}
