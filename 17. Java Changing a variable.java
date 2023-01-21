ffd468e9d3e64a55351f8f4fb54a8bd6d5599b03
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-21 23:55:45 +0500

    Java: Changing a variable

---
+++ 17. Java Changing a variable.java
@@ -0,0 +1,17 @@
+/*
+В коде определена переменная со значением "Brienna". Переопределите значение этой переменной и присвойте ей такую же, но в перевернутом виде.
+
+anneirB
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        var name = "Brienna";
+        // BEGIN (write your solution here)
+        name = "anneirB";
+        // END
+
+        System.out.println(name);
+    }
+}
