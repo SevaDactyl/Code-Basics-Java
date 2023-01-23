c679bdc3b174eb98bea90e4e7daecba158403c09
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-23 23:57:48 +0500

    Java: Errors when working with variables

---
+++ 19. Java Errors when working with variables.java
@@ -0,0 +1,20 @@
+/*
+Найдите в программе необъявленную переменную и объявите ее, присвоив ей значение "Dragon";
+
+
+Targaryen and Dragon
+
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        var family = "Targaryen";
+
+        // BEGIN
+        var pet = "Dragon";
+        // END
+
+        System.out.println(family + " and " + pet);
+    }
+}
