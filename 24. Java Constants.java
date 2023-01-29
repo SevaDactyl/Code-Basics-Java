091c7b3d57be76528f10940f7bcb48a5d3e3a497
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-29 23:53:36 +0500

    Java: Constants

---
+++ 24. Java Constants.java
@@ -0,0 +1,12 @@
+/*
+Создайте константу DRAGONS_BORN_COUNT и запишите в неё число 3 — это количество драконов, родившихся у Дайенерис. Выводить значение на экран не требуется.
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        final var DRAGONS_BORN_COUNT = 3;
+        // END
+    }
+}
