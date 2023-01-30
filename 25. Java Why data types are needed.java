973cb735e14770c4a1b3871893b6a2c9b67a1a9d
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-30 23:44:07 +0500

    Java: Why data types are needed

---
+++ 25. Java Why data types are needed.java
@@ -0,0 +1,12 @@
+/*
+Выведите на экран результат деления 3 на 2. Подумайте почему получился такой ответ?
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println(3 / 2);
+        // END
+    }
+}
