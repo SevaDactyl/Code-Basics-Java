b626b6c98925c6c86f4f6a493112039c2437d050
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-31 23:45:56 +0500

    Java: Explicit Typing

---
+++ 26. Java Explicit Typing.java
@@ -0,0 +1,13 @@
+/*
+Создайте строку One more time с явным указанием типа и выведите ее на экран
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN
+        String str = "One more time";
+        System.out.println(str);
+        // END
+    }
+}
