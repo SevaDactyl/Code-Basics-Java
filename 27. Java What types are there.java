10e3c8b475ce90ae6f31e9ee09e13312e0b76db7
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-02-01 23:50:58 +0500

    Java: What types are there

---
+++ 27. Java What types are there.java
@@ -0,0 +1,13 @@
+/*
+Выведите на экран результат конкатенации слова hexlet, символа - и числа 7
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        final var hexlet = '7';
+        System.out.println("hexlet" + "-" + hexlet);
+        // END
+    }
+}
