eafac0464c33c2215cb69aea2f7f9ddb189d89eb
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-02-02 23:37:58 +0500

    Java: Null value

---
+++ 28. Java Null value.java
@@ -0,0 +1,15 @@
+/*
+Определите переменную с именем greeting, но не инициализируйте ее
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN
+        String greeting;
+        // END
+
+        greeting = "Hello Hexlet!";
+        System.out.println(greeting);
+    }
+}
