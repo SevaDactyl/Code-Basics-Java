cac0fae9707890974eb7701b800f4ec942ba7d50
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-02-03 23:48:05 +0500

    Java: Explicit Type conversion

---
+++ 29. Java Explicit Type conversion.java
@@ -0,0 +1,12 @@
+/*
+Выведите на экран строку 2 times, полученную из числа 2.9 и строки times, используя преобразования типов и конкатенацию.
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+        // BEGIN
+        System.out.println((int) 2.9 + " times");
+        // END
+    }
+}
