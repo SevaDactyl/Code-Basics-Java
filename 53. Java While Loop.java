a41b94380dbde05ff50f2dcc3f7c328d89f0b8a2
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-03-02 23:19:42 +0500

    Java: While Loop

---
+++ 53. Java While Loop.java
@@ -0,0 +1,26 @@
+/*
+Модифицируйте метод printNumbers() так, чтобы она выводила числа в обратном порядке. Для этого нужно идти от верхней границы к нижней. То есть счётчик должен быть инициализирован максимальным значением, а в теле цикла его нужно уменьшать до нижней границы.
+
+Пример вызова и вывода:
+
+printNumbers(4);
+  4
+3
+2
+1
+finished!
+*/
+
+
+public class App {
+    public static void printNumbers(int firstNumber) {
+        // BEGIN (write your solution here)
+            int i = firstNumber;
+            while (i > 0) {
+                System.out.println(i);
+                i -= 1;
+            }
+        System.out.println("finished!");
+        // END
+    }
+}
